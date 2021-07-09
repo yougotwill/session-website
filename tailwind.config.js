1b02b86e01905c0ad167c2f86ce3d43c2e3f98c0
William Grant <willmgrant@gmail.com>
2021-07-09 13:10:33 +0200

    added Public Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['PublicSans, sans-serif'],
+    },
     extend: {
       borderWidth: {
         3: '3px',
@@ -9,6 +12,7 @@ module.exports = {
       colors: {
         primary: '#00f782',
         gray: {
+          lighter: '#7A7A7A',
           light: '#55595c',
           DEFAULT: '#3a3a3a',
           dark: '#333132',
