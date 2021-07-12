fbe56c0c65acfa4b0fa575ab5fc92be15f9cc829
William Grant <willmgrant@gmail.com>
2021-07-12 13:51:41 +0200

    basic 404 page added

---
+++ pages/404.tsx
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+
+import Layout from '@components/Layout';
+
+export default function Custom404() {
+  return (
+    <Layout title="Page not found - Session">
+      <section
+        className={classNames(
+          'py-16 px-2 mx-auto text-center',
+          'md:py-16 md:px-6',
+          'lg:p-24'
+        )}
+      >
+        <h1
+          className={classNames(
+            'text-primary-dark text-5xl font-semibold mb-8'
+          )}
+        >
+          This page doesn't seem to exist.
+        </h1>
+        <p className={classNames('text-gray text-xl', 'lg:text-2xl')}>
+          It looks like the link pointing here was faulty. Maybe try searching?
+        </p>
+        {/* TODO add search box */}
+      </section>
+    </Layout>
+  );
+}
