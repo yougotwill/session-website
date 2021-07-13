2f14da9c3025f6e96865137c560b8f90e0e5e850
William Grant <willmgrant@gmail.com>
2021-07-13 08:59:43 +0200

    tablet styling done

--- pages/download.tsx
+++ pages/download.tsx
@@ -11,7 +11,10 @@ import LinuxSVG from '@assets/svgs/linux.svg';
 import WindowsSVG from '@assets/svgs/windows.svg';
 
 export default function Download(): ReactElement {
-  const downloadLinkClasses = 'text-lg font-semibold mr-4';
+  const downloadLinkClasses = classNames(
+    'text-lg font-semibold mr-4',
+    'md:text-3xl'
+  );
   const downloadSVGClasses = 'inline-block mx-3 -mt-2 fill-current';
   return (
     <Layout title="Download - Session Private Messenger">
@@ -25,7 +28,7 @@ export default function Download(): ReactElement {
             Download Session for
           </p>
           <h2 className={classNames('text-5xl font-semibold my-4')}>Mobile</h2>
-          <div className={classNames('px-28 mb-6', 'md:px-10', 'lg:hidden')}>
+          <div className={classNames('px-28 mb-6', 'md:px-56', 'lg:hidden')}>
             <Image
               src="/assets/images/mockup-landing.png"
               alt="mobile app screenshot"
@@ -37,14 +40,17 @@ export default function Download(): ReactElement {
           <div
             className={classNames(
               'flex flex-wrap justify-center items-center pb-12',
-              'md:mb-12',
               'lg:hidden'
             )}
           >
             <Link href="/android">
               <a className={downloadLinkClasses}>
                 <AndroidSVG
-                  className={classNames(downloadSVGClasses, 'w-6 h-6')}
+                  className={classNames(
+                    downloadSVGClasses,
+                    'w-6 h-6',
+                    'md:w-8 md:h-8'
+                  )}
                   title="Android logo"
                 />
                 <span>Android</span>
@@ -53,7 +59,11 @@ export default function Download(): ReactElement {
             <Link href="https://github.com/loki-project/session-android/releases">
               <a className={downloadLinkClasses}>
                 <AndroidSVG
-                  className={classNames(downloadSVGClasses, 'w-6 h-6')}
+                  className={classNames(
+                    downloadSVGClasses,
+                    'w-6 h-6',
+                    'md:w-8 md:h-8'
+                  )}
                   title="Android logo"
                 />
                 <span>APK</span>
@@ -62,7 +72,11 @@ export default function Download(): ReactElement {
             <Link href="/iphone">
               <a className={downloadLinkClasses}>
                 <AppleSVG
-                  className={classNames(downloadSVGClasses, 'w-4 h-4')}
+                  className={classNames(
+                    downloadSVGClasses,
+                    'w-4 h-4',
+                    'md:w-6 md:h-6'
+                  )}
                   title="Apple logo"
                 />
                 <span>iPhone</span>
@@ -79,7 +93,7 @@ export default function Download(): ReactElement {
           <h2 className={classNames('text-5xl font-semibold mt-4 mb-6')}>
             Desktop
           </h2>
-          <div className={classNames('px-3 mb-6', 'md:px-10', 'lg:hidden')}>
+          <div className={classNames('px-3 mb-6', 'md:mb-10', 'lg:hidden')}>
             <Image
               src="/assets/images/mockup-desktop.png"
               alt="desktop app screenshot"
@@ -91,14 +105,18 @@ export default function Download(): ReactElement {
           <div
             className={classNames(
               'flex flex-wrap justify-center items-center pb-12',
-              'md:mb-12',
+              'md:pb-32',
               'lg:hidden'
             )}
           >
             <Link href="/mac">
               <a className={downloadLinkClasses}>
                 <AppleSVG
-                  className={classNames(downloadSVGClasses, 'w-4 h-4')}
+                  className={classNames(
+                    downloadSVGClasses,
+                    'w-4 h-4',
+                    'md:w-6 md:h-6'
+                  )}
                   title="Apple logo"
                 />
                 <span>Mac</span>
@@ -107,7 +125,11 @@ export default function Download(): ReactElement {
             <Link href="/windows">
               <a className={downloadLinkClasses}>
                 <WindowsSVG
-                  className={classNames(downloadSVGClasses, 'w-4 h-4')}
+                  className={classNames(
+                    downloadSVGClasses,
+                    'w-4 h-4',
+                    'md:w-6 md:h-6'
+                  )}
                   title="Windows logo"
                 />
                 <span>Windows</span>
@@ -116,7 +138,11 @@ export default function Download(): ReactElement {
             <Link href="/linux">
               <a className={downloadLinkClasses}>
                 <LinuxSVG
-                  className={classNames(downloadSVGClasses, 'w-5 h-5')}
+                  className={classNames(
+                    downloadSVGClasses,
+                    'w-5 h-5',
+                    'md:w-7 md:h-7'
+                  )}
                   title="Linux logo"
                 />
                 <span>Linux</span>
