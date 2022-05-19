e7c3607e51199eb1f607a6dc4686ce6e73918237
William Grant <willmgrant@gmail.com>
2022-05-19 11:18:59 +1000

    fix: banner spacing on bigger screens

--- components/ui/Banner/index.tsx
+++ components/ui/Banner/index.tsx
@@ -11,13 +11,16 @@ export default function Banner(): ReactElement {
     <div
       className={classNames(
         'bg-gray-dark text-white py-4 px-8 flex flex-col justify-center items-center leading-relaxed',
-        'lg:flex-row lg:items-start'
+        'lg:flex-row lg:items-start',
+        '2xl:items-center'
       )}
     >
       <span className={classNames('text-center mb-4', 'lg:text-left lg:mb-0')}>
         {isSmall ? BANNER.TEXT.MOBILE : BANNER.TEXT.DESKTOP}
       </span>
-      <span className={classNames('flex justify-center items-center')}>
+      <span
+        className={classNames('flex justify-center items-center', '2xl:ml-4')}
+      >
         <Link href="/download">
           <a>
             <Button
