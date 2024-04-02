3429ff15b50fe499eba2715a84fd9cbf5da7774d
Ryan Miller <ryan.m@getsession.org>
2024-04-02 14:30:22 +1100

    feat: update banner copy and button

--- components/ui/Banner/index.tsx
+++ components/ui/Banner/index.tsx
@@ -10,36 +10,25 @@ export default function Banner(): ReactElement {
   return (
     <div
       className={classNames(
-        'bg-gray-dark text-white py-4 px-8 flex flex-col justify-center items-center leading-relaxed',
-        'lg:flex-row lg:items-start',
+        'bg-gray-dark text-white py-4 px-8 flex flex-col justify-center items-center leading-relaxed align-middle',
+        'lg:flex-row',
         '2xl:items-center'
       )}
     >
-      <span className={classNames('text-center mb-4', 'lg:text-left lg:mb-0')}>
+      <span className={classNames('text-center', 'lg:text-left lg:mb-0')}>
         {isSmall ? BANNER.TEXT.MOBILE : BANNER.TEXT.DESKTOP}
       </span>
       <span
         className={classNames('flex justify-center items-center', '2xl:ml-4')}
       >
-        <Link href="/download">
+        <Link href="/blog/upgrading-to-session-network">
           <a>
             <Button
               fontWeight="bold"
               size="medium"
               classes="whitespace-nowrap mx-2"
             >
-              Update now
-            </Button>
-          </a>
-        </Link>
-        <Link href="/blog/session-network-and-client-update">
-          <a>
-            <Button
-              fontWeight="bold"
-              size="medium"
-              classes="whitespace-nowrap mx-2"
-            >
-              Read more
+              Learn more
             </Button>
           </a>
         </Link>
--- constants/banner.ts
+++ constants/banner.ts
@@ -1,9 +1,9 @@
 const BANNER = {
   TEXT: {
     MOBILE:
-      'We’re upgrading our network, so it’s important for you to update to the latest version of Session.',
+      'We are upgrading to the Session Network and migrating to Session Token.',
     DESKTOP:
-      'It’s important for you to update to the latest version of Session. Over the next few weeks, we will be upgrading the Session network and ending support for older clients.',
+      'We are upgrading to the Session Network and migrating to Session Token.',
   },
 };
 
