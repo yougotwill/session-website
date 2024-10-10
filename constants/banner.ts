420280e7af941ea71b3305911590b7c28bdeb47c
yougotwill <willmgrant@gmail.com>
2024-10-10 11:52:04 +0200

    feat: updated banner copy

--- constants/banner.ts
+++ constants/banner.ts
@@ -1,9 +1,9 @@
 const BANNER = {
   TEXT: {
     MOBILE:
-      'We are upgrading to the Session Network and migrating to Session Token.',
+      'Session is upgrading to the new Session Network and migrating to Session Token',
     DESKTOP:
-      'We are upgrading to the Session Network and migrating to Session Token.',
+      'Session is upgrading to the new Session Network and migrating to Session Token.',
   },
 };
 
