90e078f496b921301fd820c573a8540054952940
Ryan Miller <aerilym@pm.me>
2024-11-27 15:24:41 +1100

    feat: create url parser

--- utils/links.ts
+++ utils/links.ts
@@ -1,4 +1,6 @@
 import { Block, Inline } from '@contentful/rich-text-types';
+import METADATA from '../constants/metadata';
+import * as fs from 'node:fs';
 
 // utils for checking if hyperlinks are for the current site
 // are there any links to id's i.e. #mac, #linux, #windows
@@ -27,4 +29,25 @@ export function hasLocalID(node: Block | Inline) {
     }
   });
   return id;
+}
+
+const host = new URL(METADATA.HOST_URL).host;
+
+export function parseUrl(urlStr: string) {
+  try {
+    // new URL will throw if the url is invalid eg: no protocol
+    return new URL(urlStr)
+  } catch {
+    if (urlStr.startsWith(host) || urlStr.startsWith(`www.${host}`)) {
+      // We can fix the url if it's the same as the host url
+      try {
+        return new URL(`https://${urlStr}`)
+      }
+      catch {
+        return urlStr;
+      }
+    } else {
+      return urlStr;
+    }
+  }
 }
